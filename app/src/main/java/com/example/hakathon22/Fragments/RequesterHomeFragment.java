503755171d4807a6db429c3630a39f5e66f88421
ElhamImdad <Elham.imdad@gmail.com>
2019-07-22 00:30:27 +0300

    ok

--- app/src/main/java/com/example/hakathon22/Fragments/RequesterHomeFragment.java
+++ app/src/main/java/com/example/hakathon22/Fragments/RequesterHomeFragment.java
@@ -11,17 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hakathon22.R;
-public class RegistrationUserR extends Fragment {
 
-    public RegistrationUserR() {
+public class RequesterHomeFragment extends Fragment {
+
+    public RequesterHomeFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_user_r, container, false);
+        return inflater.inflate(R.layout.fragment_requester_home, container, false);
     }
-
 }
