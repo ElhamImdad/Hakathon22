0fdf9b22490e74ba7895956023225b1e0944a170
ElhamImdad <Elham.imdad@gmail.com>
2019-07-22 01:09:28 +0300

    add start page..

--- app/src/main/java/com/example/hakathon22/Activities/MainActivity.java
+++ app/src/main/java/com/example/hakathon22/Activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.hakathon22.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.hakathon22.Fragments.SelectTypeUserFragment;
 import com.example.hakathon22.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFragment(new SelectTypeUserFragment());
     }
+    private void setFragment( Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.container,fragment);
+        ft.commit();}
 }
