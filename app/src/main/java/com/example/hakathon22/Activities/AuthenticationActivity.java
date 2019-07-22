e4060081dc6ac38bce5970d4913c934d52fdbb1c
Nuhaa Alshawi <nuhaa95@hotmail.com>
2019-07-22 13:05:37 +0300

    Connect main  activiy with authentication activity

--- app/src/main/java/com/example/hakathon22/Activities/AuthenticationActivity.java
+++ app/src/main/java/com/example/hakathon22/Activities/AuthenticationActivity.java
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hakathon22.Fragments.SelectTypeUserFragment;
+import com.example.hakathon22.Helpers.SharedPrefManager;
 import com.example.hakathon22.R;
 
 public class AuthenticationActivity extends AppCompatActivity{
@@ -17,6 +19,10 @@ public class AuthenticationActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
+        if (SharedPrefManager.getInstance(this).isLoggedIn()){
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+        }
         setFragment(new SelectTypeUserFragment());
     }
     private void setFragment( Fragment fragment) {
